cf174f07114f16469811062c85d65649702be075
higadev1140 <higadev1140@gmail.com>
2025-02-16 12:22:27 +0000

    ローカル実行

--- src/lib/dataSelector.ts
+++ src/lib/dataSelector.ts
@@ -1,13 +1,15 @@
 
 import { getDataFromGcloud, putDataFromGcloud } from "./gcloudData"
-
-
+import { getMockData, putMockData } from "./mockData";
 
 export const getDataFunc = () => {
-  return getDataFromGcloud;
+  return isProd() ? getDataFromGcloud : getMockData;
 }
 
 export const putDataFunc = () => {
+  return isProd() ?  putDataFromGcloud : putMockData;
+}
 
-  return putDataFromGcloud;
+const isProd = () =>{
+  return process.env.NEXT_PUBLIC_ENV=== "prod";
 }
